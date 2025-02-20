6fe806a93b6180e6868fb8d9db0d5fd15da5ec54
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-02-20 20:36:19 +0100

    controller Pokemons

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
-  has_many :pokeballs
+  has_many :pokeballs, dependent: :destroy
+  has_many :trainers, through: :pokeballs
   has_one_attached :photo
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,5 @@
 class Trainer < ApplicationRecord
-  has_many :pokeballs
+  has_many :pokeballs, dependent: :destroy
+  has_many :pokemons, through: :pokeballs
   has_one_attached :photo
 end
