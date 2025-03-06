f2a581e1ea70ede6ae20a12d0be2a58d32446b04
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-03-06 14:23:19 +0100

    New et Create Trainers

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -7,4 +7,18 @@ class TrainersController < ApplicationController
   def show
     @trainer = Trainer.find(params[:id])
   end
+
+  def new
+    @trainer = Trainer.new
+  end
+
+  def create
+    @trainer = Trainer.new(trainer_params)
+  end
+
+  private
+
+  def trainer_params
+    params.require(:trainer).permit(:name, :age, :photo)
+  end
 end
